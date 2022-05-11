679bdbcbee923983a7c3c491e6faf7cffb318cb9
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-05-11 23:24:25 +0530

    deleteNotes functionality

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,11 +1,15 @@
 import Note from './Note';
 import AddNote from './AddNote';
 
-const NotesList = ({ notes, handleAddNote }) => {
+const NotesList = ({ notes, handleAddNote, handleDeleteNote }) => {
     return (
         <div className='notes-list'>
             {notes.map((note, index) => (
-                <Note note={note} key={note + index} />
+                <Note
+                    note={note}
+                    key={note + index}
+                    handleDeleteNote={handleDeleteNote}
+                />
             ))}
 
             <AddNote handleAddNote={handleAddNote} />
